9a5ee1e45ac7eb2ee382df160a9eb91be496b1b0
Ojini Chizoba Jude <judoc96@gmail.com>
2018-10-31 21:34:42 +0100

    server adjustment

--- api/app.js
+++ api/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "client", "build")));
 
-app.use("/api", todoRoutes);
+// app.use("/api", todoRoutes);
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const http = require("http");
 const app = require("./api/app");
 
+const port = process.env.PORT || 4000;
+
 const server = http.createServer(app);
 
-const port = process.env.PORT || 4000;
 server.listen(port, () => {
   console.log(`server now running on port ${port}`);
 });
